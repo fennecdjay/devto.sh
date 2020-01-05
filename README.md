119c749ff96e4fde730c79b8f8f4b1395a62a79c
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-01-05 18:03:39 +0100

    :art: First draw

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # devto.sh
-Leverage bash and github actions to automate posting to dev.to
+
+Leverage bash and github actions to automate posting to [dev.to](https://dev.to)
+
+## Bash script
+
+The script takes 2 arguments:
+  * the *source* of the blog
+  * the dev.to *API_KEY*
+
+it will update (or create if necessary) any markdown file
+in the *source* directory.
+
+## Github Action
+
+  > The DEVTO_API_KEY has to be set in your repository settings.
+  see [here](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)
+
+
+When triggered (when you push),
+the action will run the script to update your [dev.to](https://dev.to) account
+
+## Default settings
+
+the *source* directory is `src`
+the *markdown extension* is `.md`
+the API_KEY is named 'DEVTO_API_KEY'
+
---
+++ devto.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SRC_DIR="$1"
+API_KEY="$2"
+MKD_EXT=".md"
+
+check() {
+  if [ "$3" = "null" ]
+  then
+    echo "😡: error $1 post '$2'"
+    return 1
+  fi
+  echo "😊: success $1 post '$2'"
+  return 0
+}
+
+send() {
+  curl -X "$1" -H "Content-Type: application/json" \
+     -H "api-key: $API_KEY" \
+     -d "$(file2json "$2")" \
+     "https://dev.to/api/articles/$3" 2>/dev/null
+}
+
+create() {
+  id=$(send "POST" "$1" | jq -r ".id")
+  check "creating" "$1" "$id" && echo "$id" > "$2"
+}
+
+update() {
+  id=$(send "PUT" "$1" "$(cat "$2")" | jq -r ".id")
+  check "updating" "$1" "$id"
+}
+
+file2json() {
+  var=$(cat "$1")
+  jq -n --arg v "$var" '{"article":{"body_markdown":$v}}'
+}
+
+for post in "$SRC_DIR/"*"$MKD_EXT"
+do
+  id="${post:0:-2}id"
+  if [ -f "$id" ]
+  then update "$post" "$id"
+  else create "$post" "$id"
+  fi
+done
